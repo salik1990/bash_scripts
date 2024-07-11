22cb841ceda4851e9059c0b2953a7370091300d1
salik1990 <salik7ee@gmail.com>
2024-07-11 12:03:09 +0500

    modified hello.sh

--- hello.sh
+++ hello.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
 echo "Helle There"
+echo "Shell scripting is a powerful way to automate tasks and manage system operations through a series of command-line instructions written in a script file. It enables users to create scripts for repetitive tasks, system monitoring, and software management, enhancing efficiency and productivity in Unix-based systems."
